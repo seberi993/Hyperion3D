20f6260950ab917ab942db058fdddd01366da3e2
sebastian.eriksson@sectoralarm.se <sebastian.eriksson@sectoralarm.se>
2022-11-07 11:08:07 +0100

    react router installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+function Home() {
+  return (
+    <>
+      <main>
+        <div className="App">
+          <div className="card">
+            <header>
+              <img src="src\img\logo_trans.png" width="100%" />
+            </header>
+          </div>
+        </div>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <p className="read-the-docs">
+        © 2022 Hyperion 3D Inc All Rights Reserved
+      </p>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          We believe the future is in printing, and we can print your dreams
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 
 function App() {
-  
-
   return (
-    <div className="App">
-      <div className="card" >
-        <header>
-        <button>Products</button>
-        <button>Gallery</button>
-        <button>Contact</button>
-        <img src="src\img\logo_trans.png" width="100%"/>
-        </header>
-
-        <button>Privacy & Security</button>
-      <button>Legal & Trademarks</button>
-      <button>Terms of Service</button>
-      </div>
-      <p className="read-the-docs">© 2022 Hyperion 3D Inc All Rights Reserved</p>
-     
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
   );
 }
 
